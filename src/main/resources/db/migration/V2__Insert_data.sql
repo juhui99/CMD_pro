6a6f09be24eb3ac9ae888aae0de13536873aed13
SON <hochan10@naver.com>
2021-07-06 22:18:42 +0900

    마이그레이션 안되는 것 수정

--- src/main/resources/db/migration/V2__Insert_data.sql
+++ src/main/resources/db/migration/V2__Insert_data.sql
@@ -8,3 +8,14 @@ insert into users ( `user_index`,
                     `user_gender`,
                     `user_profile`)
 values(1, 'juhee', '1234','이주희', '23', 'test', 'juhee@gmail.com', 'W', 'profile');
+
+insert into users ( `user_index`,
+                    `user_id`,
+                    `user_pwd`,
+                    `user_name`,
+                    `user_age`,
+                    `user_major`,
+                    `user_email`,
+                    `user_gender`,
+                    `user_profile`)
+values(2, 'juhee', '1234','이주희', '23', 'test', 'juhee@gmail.com', 'W', 'profile');
