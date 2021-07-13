296ea65e9fd8c494fcb6911df8687a981e7d8527
주희 <dlwngml21@gmail.com>
2021-07-13 17:05:38 +0900

    docker DB 구축 테스트

--- src/main/resources/db/migration/V1__Create_table.sql
+++ src/main/resources/db/migration/V1__Create_table.sql
@@ -4,15 +4,16 @@ CREATE TABLE `testest` (
                         PRIMARY KEY (`index`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `users3` (
+CREATE TABLE `users` (
                          `user_index` int(11) NOT NULL,
-                         `user_id` varchar(10) DEFAULT NULL,
-                         `user_pwd` varchar(20) DEFAULT NULL,
+                         `user_id` varchar(10) NOT NULL,
+                         `user_pwd` varchar(20) NOT NULL,
                          `user_name` varchar(80) DEFAULT NULL,
                          `user_age` varchar(20) DEFAULT NULL,
-                         `user_major`varchar(20) DEFAULT NULL,
-                         `user_email`varchar(200) DEFAULT NULL,
-                         `user_gender`varchar(20) DEFAULT NULL,
-                         `user_profile`varchar(20) DEFAULT NULL,
+                         `user_major` varchar(20) DEFAULT NULL,
+                         `user_email` varchar(200) DEFAULT NULL,
+                         `user_gender` varchar(20) DEFAULT NULL,
+                         `user_profile` varchar(20) DEFAULT NULL,
+                         `user_manage` int NOT NULL,
                          PRIMARY KEY (`user_index`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
