ae7882a22e601a74044792a824e61bab9ea9c524
주희 <dlwngml21@gmail.com>
2021-08-11 02:26:52 +0900

    survey ing...

--- src/main/java/com/CMD/CMD_pro/survey/service/servicempl/SurveyServicempl.java
+++ src/main/java/com/CMD/CMD_pro/survey/service/servicempl/SurveyServicempl.java
@@ -12,32 +12,6 @@ import java.util.List;
 @Service
 public class SurveyServicempl implements SurveyService {
 
-//    @Autowired
-//    SurveyVO surveyVO;
-    @Autowired
-    private SqlSession sqlSession;
-
-    private  String namespace = "com.CMD.CMD_pro.mapper.SurveyMapper";
-
-    @Override
-    public List<SurveyVO> getSurveyList() throws Exception {
-        List<SurveyVO> surveyList = sqlSession.selectList(namespace+".selectSurveyList");
-        return surveyList;
-    }
-
-//    @Override
-//    public SurveyVO getSurveyVO(int surveyIndex) throws Exception{
-//        return sqlSession.selectOne(namespace+".selectSurvey", surveyIndex);
-//    }
-
-    @Override
-    public SurveyWithItemVO getSurveyItems(int surveyIndex) throws Exception {
-        SurveyVO surveyVO = getSurvey(surveyIndex);
-        SurveyWithItemVO surveyWithItemVO = new SurveyWithItemVO(surveyVO);
-       // SurveyWithItemVO.setSurveyItemList(dao.selectSurveyItems(surveyIndex));
-//		surveyWithItemVO.setMySurvey(dao.selecyMySurveyResult(survey_seq, member_seq));
-        return surveyWithItemVO;
-    }
 
 
 
